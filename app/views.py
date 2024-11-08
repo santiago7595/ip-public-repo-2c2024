d6067cc309a1a501456ac0d9d12c4b3ade17df63
Santiago7595 <santiagoc7595@gmail.com>
2024-11-08 18:29:58 -0300

    nueva prueba de comit

--- app/views.py
+++ app/views.py
@@ -11,7 +11,7 @@ def index_page(request):
 # esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
 # si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
 def home(request):
-    images = []
+    images = [] #prueba de otro commit
     favourite_list = []
 
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
