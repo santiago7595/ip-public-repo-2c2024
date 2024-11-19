da4257d53fb44077e20d704e3de5bf5a377141ee
luciano1-ux <lucianoferrando0@gmail.com>
2024-11-19 18:05:47 -0300

    importe la funcion getallimges

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect, render
 from .layers.services import services
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from app.layers.services.services import getAllImages
 
 def index_page(request):
     return render(request, 'index.html')
